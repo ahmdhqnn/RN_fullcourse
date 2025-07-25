19a9f712e4264cb8c0b49f3ec1bf8563207801f8
Ahmad Hakin Najili <ahmadhaqinn@gmail.com>
2025-07-25 08:37:35 +0700

    navigation links

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,29 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.view}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/exampleScreen" style={styles.navButton}>Example Screen</Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  navButton: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: "#007AFF",
+    borderRadius: 5,
+  }
+});
+
+
