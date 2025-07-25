95f4ee6e8f3080c28e3bf79544d3ad5e53ab4242
Ahmad Hakin Najili <ahmadhaqinn@gmail.com>
2025-07-25 19:24:41 +0700

    signup / signin logic, ui

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,26 @@
+import { Stack, useRouter } from "expo-router";
 
+import React, { useEffect } from "react";
 
-import { Stack } from "expo-router";
+function RouteGuard({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const isAuth = false;
+
+  useEffect(() => {
+    if (!isAuth) {
+      router.replace("/auth",);
+    }
+  });
+
+  return <>{children}</>;
+}
 
 export default function RootLayout() {
-  return(
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }}/>
-      
-    </Stack>
+  return (
+    <RouteGuard>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </RouteGuard>
   )
 }
